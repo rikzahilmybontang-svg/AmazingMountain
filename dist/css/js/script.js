e44754c1f10261fef3284fc8d38713c2456e567e
rikzahilmybontang-svg <rikzahilmybontang@gmail.com>
2026-05-20 23:46:09 +0800

    Add files via upload

---
+++ dist/css/js/script.js
@@ -0,0 +1,5 @@
+const menuBar = document.querySelector(".menu-bar");
+const menuNav = document.querySelector(". menu-navigation");
+menuBar.addEventListener('click' , function(){
+    menuNav.classList.toggle('menu-active');
+})
